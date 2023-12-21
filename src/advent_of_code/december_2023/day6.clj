d7c55e87a09a8594e3d74f1baec52137adf47d86
James Kent <jkent@chariotsolutions.com>
2023-12-21 15:07:50 -0500

    day 6, problem 2

--- src/advent_of_code/december_2023/day6.clj
+++ src/advent_of_code/december_2023/day6.clj
@@ -2,18 +2,34 @@
   (:require [advent-of-code.tools :as tools]
             [clojure.string :as str]))
 
+(defn remove-labels [x]
+  (second (str/split x #":\s+")))
+
+(defn transpose [xs]
+  (->> (apply interleave xs)
+       (partition 2)))
+
 (defn find-number-of-winning-durations [[race-length record-distance]]
   (->> (range 1 (inc race-length))
        (filter (fn [hold-duration]
-                 (> (-> (- race-length hold-duration)
-                        (* hold-duration))
+                 (> (* (- race-length hold-duration)
+                       hold-duration)
                     record-distance)))
        count))
 
 (defn problem-1 []
   (->> (tools/read-file-lines "resources/december-2023/day6")
-       (map #(tools/read-vector-string (second (str/split % #":\s+"))))
-       (apply interleave)
-       (partition 2)
+       (map #(tools/read-vector-string (remove-labels %)))
+       transpose
        (map find-number-of-winning-durations)
        (apply *)))
+
+(defn remove-whitespace [s]
+  (str/replace s #"\s+" ""))
+
+(defn problem-2 []
+  (->> (tools/read-file-lines "resources/december-2023/day6")
+       (map #(tools/read-vector-string (remove-whitespace (remove-labels %))))
+       transpose
+       (map find-number-of-winning-durations)
+       first))

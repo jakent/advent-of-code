491a633cafca0631b8a19617629ebf263b7aa1cf
James Kent <jkent@chariotsolutions.com>
2023-12-21 21:59:10 -0500

    day 8, problem 2

--- src/advent_of_code/december_2023/day8.clj
+++ src/advent_of_code/december_2023/day8.clj
@@ -8,23 +8,52 @@
 (defn parse-lookup [raw]
   (reduce (fn [acc x]
             (let [[k v] (str/split x #" = ")
-                  [left right] (map str (read-string v))]
+                  [_ left right] (re-matches #"\((\S{3}), (\S{3})\)" v)]
               (assoc acc k {:left left :right right})))
           {}
           raw))
 
-(defn navigate-the-haunted-wasteland [instructions lookup]
+(defn navigate-the-haunted-wasteland [{:keys [instructions lookup]} start exit-condition]
   (reduce (fn [[i current-location] direction]
             (let [destination (direction (get lookup current-location))]
-              (if (= "ZZZ" destination)
+              (if (exit-condition destination)
                 (reduced (inc i))
                 [(inc i) destination])))
-          [0 "AAA"]
+          [0 start]
           (cycle instructions)))
+
+(defn parse [filename]
+  (let [[instructions _ & rest] (tools/read-file-lines filename)]
+    {:instructions (parse-instructions instructions)
+     :lookup       (parse-lookup rest)}))
 
 (defn problem-1
   "20569"
   []
-  (let [[instructions _ & rest] (tools/read-file-lines "resources/december-2023/day8")]
-    (navigate-the-haunted-wasteland (parse-instructions instructions)
-                                    (parse-lookup rest))))
+  (navigate-the-haunted-wasteland (parse "resources/december-2023/day8")
+                                  "AAA"
+                                  (partial = "ZZZ")))
+
+(defn -gcd [x y]
+  (if (zero? y)
+    x
+    (recur y (mod x y))))
+
+(defn gcd [xs]
+  (reduce -gcd xs))
+
+(defn lcm [xs]
+  (reduce (fn [x y]
+            (* x (/ y (gcd [x y]))))
+          1
+          xs))
+
+(defn problem-2
+  "21366921060721"
+  []
+  (let [{:keys [lookup] :as parsed} (parse "resources/december-2023/day8")]
+    (->> (filterv #(str/ends-with? % "A") (keys lookup))
+         (map #(navigate-the-haunted-wasteland parsed
+                                               %
+                                               (fn [x] (str/ends-with? x "Z"))))
+         lcm)))

353d9471047b18a3dc125a1d14eb37a155ed2554
James Kent <jkent@chariotsolutions.com>
2023-12-22 10:37:17 -0500

    day 9, problem 2

--- src/advent_of_code/december_2023/day9.clj
+++ src/advent_of_code/december_2023/day9.clj
@@ -3,7 +3,8 @@
 
 (defn calculate-differences [[f & rest]]
   (-> (reduce (fn [[a acc] b]
-                [b (conj acc (- b a))])
+                ;[b (conj acc (- b a))]) -- problem 1
+                [b (conj acc (- a b))])
               [f []]
               rest)
       second))
@@ -15,15 +16,31 @@
 
 (defn extrapolate [xss]
   (-> (reduce (fn [[l v] xs]
-                (let [last (+ l (last xs))]
-                  [last (conj v (conj xs last))]))
+                ;(let [last (+ l (last xs))]
+                ;  [last (conj v (conj xs last))])) -- problem 1
+                (let [last (+ l (first xs))]
+                  [last (conj v (cons last xs))]))
               [0 []]
               xss)
       second))
 
-(defn problem-1 []
+(defn problem-1
+  "1930746032"
+  []
   (->> (tools/read-file-lines "resources/december-2023/day9")
        (map (comp last
+                  last
+                  extrapolate
+                  find-zero-differences
+                  list
+                  tools/read-vector-string))
+       (apply +)))
+
+(defn problem-2
+  "1154"
+  []
+  (->> (tools/read-file-lines "resources/december-2023/day9")
+       (map (comp first
                   last
                   extrapolate
                   find-zero-differences

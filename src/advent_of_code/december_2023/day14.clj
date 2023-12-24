0e97fef72e928250df606df19296d4082f7c30f4
James Kent <jkent@chariotsolutions.com>
2023-12-24 11:04:24 -0500

    day 14, problem 2 - much too slow

--- src/advent_of_code/december_2023/day14.clj
+++ src/advent_of_code/december_2023/day14.clj
@@ -1,13 +1,13 @@
 (ns advent-of-code.december-2023.day14
   (:require [advent-of-code.tools :as tools]))
 
-(defn tilt [row]
+(defn tilt [comp row]
   (->> row
        (partition-by (partial = \#))
        (mapcat (fn [g]
                  (if (= \# (first g))
                    g
-                   (sort-by int > g))))))
+                   (sort-by int comp g))))))
 
 (defn score [dish]
   (-> (reduce (fn [[points total] row]
@@ -24,6 +24,22 @@
   []
   (->> (tools/read-file-lines "resources/december-2023/day14")
        tools/transpose
-       (map tilt)
+       (map (partial tilt >))
        tools/transpose
        score))
+
+(defn cycle-dish [dish]
+  (->> dish
+       tools/transpose
+       (map (partial tilt >))
+       tools/transpose
+       (map (partial tilt >))
+       tools/transpose
+       (map (partial tilt <))
+       tools/transpose
+       (map (partial tilt <))))
+
+(-> (reduce (fn [dish _] (cycle-dish dish))
+            (tools/read-file-lines "resources/december-2023/day14")
+            (range 1000000000))
+    score)
